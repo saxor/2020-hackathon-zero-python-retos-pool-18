1b7921b23813981486c26177cb0d8f00302eb5e8
sxr <sxrs2s2@gmail.com>
2020-05-16 18:58:39 +0200

    kata2

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -4,10 +4,10 @@ import random
 import string
 
 def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
-    #
-    #
+    result = str(random.choice(string.ascii_letters)) + str(random.choice(string.digits)) + str(random.choice(string.punctuation))
+    for len in range(passLen - 3): 
+        result += random.choice([str(random.choice(string.ascii_letters)), str(random.choice(string.digits)), str(random.choice(string.punctuation))])
 
-    return ""
+    return result
+
+RandomPasswordGenerator()
