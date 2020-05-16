4c1035b3c68894f5d10dd07235dcba06d3f7cdaf
sxr <sxrs2s2@gmail.com>
2020-05-16 18:31:20 +0200

    rsp

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -6,18 +6,43 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Empate!'
 #'Ganaste!'
 #'Perdiste!'
-def quienGana(player, ai):
-    return ""
+def quienGana(player, ia):
+    player = player.lower()
+    ia = ia.lower()
+    
+    plays = [
+        { 'player': 'piedra', 'ia': 'tijeras', 'res': 'G' },
+        { 'player': 'piedra', 'ia': 'papel', 'res': 'P' },
+        { 'player': 'papel', 'ia': 'piedra', 'res': 'G' },
+        { 'player': 'papel', 'ia': 'tijeras', 'res': 'P' },
+        { 'player': 'tijeras', 'ia': 'papel', 'res': 'G' },
+        { 'player': 'tijeras', 'ia': 'piedra', 'res': 'P' }]
+
+    result = [item for item in plays if item["player"] == player and item["ia"] == ia]
+    if player == ia:
+        return 'Empate!'
+    if result[0]['res'] == 'G':
+        return 'Ganaste!'
+    if result[0]['res'] == 'P':
+        return 'Perdiste!'
+    
+    return "Error"
 
 # Entry Point
 def Game():
     #
+    # Input para el jugador player
+    player = input('Introduce con que vas a jugar (Piedra, Papel o Tijeras): ')
+    player = player.lower()
+
     #
-    
-    #
-    #
-    
+    # Random para la IA
+    ai = options[randint(0,2)]
+    print(ai)
+
     winner = quienGana(player, ai)
 
     print(winner)
 
+if __name__ == '__main__':
+    Game()
